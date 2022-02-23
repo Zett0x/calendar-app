fae841d89cf9a7d31a5cbe342d3a9b09fcfb9547
Zett0x <zetto1514@gmail.com>
2022-02-23 22:29:29 +0100

    h1 del modal actualizado

--- src/components/calendar/CalendarModal.jsx
+++ src/components/calendar/CalendarModal.jsx
@@ -149,7 +149,7 @@ export const CalendarModal = () => {
       className="modal"
       overlayClassName="modal-fondo"
     >
-      <h1> Nuevo evento </h1>
+      <h1> {activeEvent ? "Editar evento" : "Nuevo evento"}</h1>
       <hr />
       <form className="container" onSubmit={handleSubmitForm}>
         <div className="form-group">
